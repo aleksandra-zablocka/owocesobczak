e08e3d70c5e9f1bd76c36b419c26b4afa22cae25
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2024-02-03 18:50:33 +0100

    added js script and modal for products

---
+++ src/scripts/product-list.js
@@ -0,0 +1,53 @@
+const productList = document.querySelector(".products__list");
+
+fetch("./src/datasource/apples.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((apple) => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("products__item");
+
+      listItem.innerHTML = `
+        <figure class="product__figure">
+          <img class="products__image" src="${apple.img}" alt="${apple.alt}" /> 
+          <figcaption class="product__figcaption">${apple.name}</figcaption>
+        </figure>
+      `;
+
+      listItem.addEventListener("click", () => {
+        showModal(apple);
+      });
+
+      productList.appendChild(listItem);
+    });
+  });
+
+function showModal(apple) {
+  const appleBackdrop = document.createElement("div");
+  appleBackdrop.classList.add("apple-backdrop");
+
+  const appleModal = document.createElement("div");
+  appleModal.classList.add("apple-modal");
+
+  appleModal.innerHTML = `
+   <span class="closeBtn">&times;</span>
+       <h3 class="apple-title">${apple.name}</h3>
+       <p>Kolor: ${apple.color}</p>
+       <p>Klasa: ${apple.class}</p>
+       <img src="${apple.img}" alt="${apple.alt}">
+     `;
+
+  appleBackdrop.addEventListener("click", (event) => {
+    if (event.target === appleBackdrop || event.target.classList.contains("closeBtn")) {
+      document.body.removeChild(appleBackdrop);
+    }
+  });
+
+  appleBackdrop.appendChild(appleModal);
+  document.body.appendChild(appleBackdrop);
+}
+
+const closeButton = appleModal.querySelector(".closeBtn");
+closeButton.addEventListener("click", () => {
+  appleModal.style.display = "none";
+});
