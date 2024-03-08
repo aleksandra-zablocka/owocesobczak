615263ff8aa14c5bcaedcb0b71563f138f712c4e
Aleksandra Zabłocka <aleksandra-zablocka@o2.pl>
2024-03-08 11:34:37 +0100

    added privacy policy and cookies message to each language

--- src/scripts/privacy-policy.js
+++ src/scripts/privacy-policy.js
@@ -1,7 +1,7 @@
 const cookies = document.querySelector(".cookies");
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (!localStorage.getItem("privacyPolicyAccepted")) {
+  if (!sessionStorage.getItem("privacyPolicyAccepted")) {
     cookies.style.display = "block";
   } else {
     cookies.style.display = "none";
@@ -9,6 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function acceptPrivacyPolicy() {
-  localStorage.setItem("privacyPolicyAccepted", "true");
+  sessionStorage.setItem("privacyPolicyAccepted", "true");
   cookies.style.display = "none";
 }
